795ea052473c00ad96d26b99b205bfddb1f5d909
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-30 17:15:06 +0100

    init problem fixed

--- _printf.c
+++ _printf.c
@@ -10,10 +10,10 @@
  */
 int _printf(const char *format, ...)
 {
-int count = 0;
+int i, count = 0;
 va_list args;
 va_start(args, format);
-for (int i = 0; format && format[i] != '\0'; i++)
+for (i = 0; format && format[i] != '\0'; i++)
 {
 if (format[i] == '%' && format[i + 1] != '\0')
 {
