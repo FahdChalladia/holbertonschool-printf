8f2baf4258c946f3ec956af40cff7e6a7587b02c
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-30 17:34:04 +0100

    getting the old version back

--- helpers.c
+++ helpers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdarg.h>
 
 /**
  * print_char - Prints a single character.
@@ -23,7 +22,6 @@ return (1);
 int print_string(char *str)
 {
 int count = 0;
-
 if (str == NULL)
 {
 write(1, "(null)", 6);
@@ -45,10 +43,8 @@ return (count);
  */
 int print_number(int n)
 {
-int j, count = 0;
+int i, j, count = 0;
 char buffer[10];
-int i = 0;
-
 if (n == 0)
 {
 write(1, "0", 1);
@@ -71,44 +67,5 @@ for (j = i - 1; j >= 0; j--)
 write(1, &buffer[j], 1);
 count++;
 }
-return (count);
-}
-
-/**
- * _printf - Prints output based on the format string.
- * @format: Format string to handle.
- *
- * Return: The number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-int i, count = 0;
-va_list args;
-
-va_start(args, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] == '%' && format[i + 1] != '\0')
-{
-i++;
-if (format[i] == 'c')
-count += print_char(va_arg(args, int));
-else if (format[i] == 's')
-count += print_string(va_arg(args, char *));
-else if (format[i] == 'd' || format[i] == 'i')
-count += print_number(va_arg(args, int));
-else if (format[i] == '%')
-{
-write(1, "%", 1);
-count++;
-}
-}
-else
-{
-write(1, &format[i], 1);
-count++;
-}
-}
-va_end(args);
 return (count);
 }
