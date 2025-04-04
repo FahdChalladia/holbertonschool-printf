34a26b857ffcffec30ab38483b9e903eba55af99
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-04 15:31:46 +0100

    changed place of % test

--- _printf.c
+++ _printf.c
@@ -14,17 +14,13 @@ int count = 0, i;
 va_list args;
 if (format == NULL )
 write(1, "", 0);
+else if (format[0] == '%' && format[1] == '\0')
+write(1, "", 0);
 
 va_start(args, format);
 for (i = 0; format[i] != '\0'; i++)
 {
-if (format[i] == '%' && format[i + 1] == '\0')
-{
-write(1, "", 0);
-va_end(args);
-return (0);
-}
-else if (format[i] == '%' && format[i + 1] != '\0')
+if (format[i] == '%' && format[i + 1] != '\0')
 {
 i++;
 count += handle_format(format[i], args);
