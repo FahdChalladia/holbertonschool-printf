059f2ef3fe884e212fa7fd0bde5f889c24592613
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-04 18:17:32 +0100

    missing changes fixed

--- main.h
+++ main.h
@@ -7,9 +7,9 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(char *s);
 int print_number(int num);
-int print_unsigned_int(unsigned num);
-int print_octal(unsigned num);
-int print_hex(unsigned num, char format);
+int print_unsigned_int(unsigned int num);
+int print_octal(unsigned int num);
+int print_hex(unsigned int num, char format);
 int print_pointer(void *ptr);
 int handle_format(char specifier, va_list args);
 
